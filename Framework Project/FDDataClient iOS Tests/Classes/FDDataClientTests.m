53bb081c859e7280a3b88704aa3283e70c9e3735
Reid Main <reidmain@gmail.com>
2015-09-11 22:28:16 -0700

    Added test targets.

---
+++ Framework Project/FDDataClient iOS Tests/Classes/FDDataClientTests.m
@@ -0,0 +1,17 @@
+@import XCTest;
+
+#import "FDDataClient.h"
+
+
+@interface FDDataClientTests : XCTestCase
+
+@end
+
+@implementation FDDataClientTests
+
+- (void)testExample
+{
+	XCTAssertNotNil([FDDataClient new]);
+}
+
+@end
